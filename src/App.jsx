5fa2a4849158c9ab1c8de564998bacd60bb7acc5
Cristian1-dev <cristianros211@gmail.com>
2025-06-30 20:22:57 -0600

    Agreando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
+
 export const App = () => {
+
+  const [tareas, setTareas] = useState('')
+  const [listaTareas, setlistaTareas] = useState([])
+
+  const agregarTarea = () => {
+    if (tareas.trim() !== '') {
+      setlistaTareas([...listaTareas, tareas])
+      setTareas('');
+    };
+  };
 
   return (
     <div className='contenedor'>
       <h1>Lista de Tareas</h1>
       <div className="entrada">
-        <input type="text" />
-        <button>Agregar</button>
+        <input type="text" placeholder='Agrege una tarea' value={tareas} onChange={(e) => setTareas(e.target.value)} />
+        <button onClick={agregarTarea}>Agregar</button>
       </div>
-      
+      <div className='lista'>
+        {listaTareas.map((tarea, index) => (
+          <div className='tarea-contenedor' key={index}>
+            <div className='parrafo'>{tarea}</div>
+            <div className='botones'>
+              <button className='eliminar'>E</button>
+              <button className='actualizar'>A</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   )
 
