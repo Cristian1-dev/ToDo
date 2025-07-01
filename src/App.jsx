74e68e31310b88650b8e6b4104499c98956dd963
Cristian1-dev <cristianros211@gmail.com>
2025-06-30 19:27:49 -0600

    Actualizacion
    Reorganizando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { Titulo } from './componentes/Titulo.jsx';
-import { Tarea } from './componentes/Tarea.jsx';
-import { Input } from './componentes/Input.jsx';
-
 import './App.css'
 
 export const App = () => {
+
   return (
-    <div className="contenedor-Tareas">
-      <Titulo titulo = "Lista de Tareas" />
-      <Input />
-      <Tarea />
-      <Tarea />
+    <div className='contenedor'>
+      <h1>Lista de Tareas</h1>
+      <div className="entrada">
+        <input type="text" />
+        <button>Agregar</button>
+      </div>
       
     </div>
   )
+
 }
--- src/componentes/Titulo.jsx
+++
@@ -1,7 +0,0 @@
-import '../estilos/Titulo.css';
-
-export const Titulo = ({ titulo }) => {
-    return (
-        <div className="titulo">{titulo}</div>
-    )
-}
